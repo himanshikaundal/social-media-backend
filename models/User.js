efa260c24928e600b9f379fbc5807118e8bba6e2
iswar021 <himanshu.rawat@tothenew.com>
2022-04-04 13:06:16 +0530

    feeds and comments schema

--- models/User.js
+++ models/User.js
@@ -5,3 +5,31 @@ const userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+
+
+const feedSchema=new mongoose.Schema({
+    content:{
+        type:String,
+        required:true,
+        maxlength:1024,
+
+          },
+    feed_id:{
+        type:String,
+        required:true,
+    }
+
+})
+
+exports.Feed=mongoose.model('Feed',feedSchema);
+
+
+const commentSchema=new mongoose.Schema({
+    comment:{
+        type:String,
+        required:true,
+        maxlength:500,
+            }
+})
+exports.Comment=mongoose.model('Comment',commentSchema);
