80a98c27234d04cde3cf137a2359470b9b2dafae
himanshikaundal <99386107+himanshikaundal@users.noreply.github.com>
2022-04-04 13:18:28 +0530

    user schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,72 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String
+    name:{
+        type:String,
+        required:true,
+        min:5,
+        max:255
+      },
+      username:{
+        type:String,
+        required:true,
+        min:5,
+        max:255,
+        unique:true
+      },
+      email:{
+        type:String,
+        required: true,
+        trim:true,
+        lowercase:true,
+        min:10,
+        max:255
+      },
+      password:{
+        type:String,
+        required:true,
+        min:5,
+        max:255,
+      },      
+      isAdmin:{
+        type:Boolean,
+        default:false
+      },
+      profilePicture:{
+        type:String,
+        default:""
+      },
+    coverPicture:{
+        type:String,
+        default:""
+      },
+    headline:{
+        type:String,
+        max:20
+    },
+    state:{
+        type:String,
+        max:20
+    },
+    county:{
+        type:String,
+        max:20
+    },
+    city:{
+        type:String,
+        max:20
+    },
+    website:{
+        type:String,
+        max:20
+    },
+    gender:{
+        type:String,
+        enum:['male','female']
+    },
+    dob:{
+        type:Date
+    }  
 });
 
 module.exports = mongoose.model('User', userSchema);
